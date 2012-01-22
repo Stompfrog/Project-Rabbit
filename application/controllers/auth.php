d7cef533715d88e0c6ec82851ec4de630530b8c8
Chris Bewick <chris@stompfrog.co.uk>
2012-01-22 11:28:43 +0000

    styling registration view

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -137,6 +137,7 @@ class Auth extends CI_Controller
 			$this->form_validation->set_rules('email', 'Email', 'trim|required|xss_clean|valid_email');
 			$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean|min_length['.$this->config->item('password_min_length', 'tank_auth').']|max_length['.$this->config->item('password_max_length', 'tank_auth').']|alpha_dash');
 			$this->form_validation->set_rules('confirm_password', 'Confirm Password', 'trim|required|xss_clean|matches[password]');
+			$this->form_validation->set_error_delimiters('<span class="help-inline">', '</span>');
 
 			$captcha_registration	= $this->config->item('captcha_registration', 'tank_auth');
 			$use_recaptcha			= $this->config->item('use_recaptcha', 'tank_auth');
@@ -193,7 +194,9 @@ class Auth extends CI_Controller
 			$data['use_username'] = $use_username;
 			$data['captcha_registration'] = $captcha_registration;
 			$data['use_recaptcha'] = $use_recaptcha;
+			$this->load->view('templates/header');
 			$this->load->view('auth/register_form', $data);
+			$this->load->view('templates/footer');
 		}
 	}
 
