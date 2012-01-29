1d308c4650dcc7a1e08464ac6dad8dbdb76cbbba
Chris Bewick <chris@stompfrog.co.uk>
2012-01-29 13:39:39 +0000

    removing <code>

--- application/views/api/index.php
+++ application/views/api/index.php
@@ -10,6 +10,6 @@
 			<li>Get the 2 latest artists as JSON = <?php echo anchor('/api/artists/2', '/api/artists/2'); ?></li>
 			<li>Get the artist with id 2 as JSON = <?php echo anchor('/api/artist/2', '/api/artist/2'); ?></li>
 		</ul>
-		<pre><code id="result">Results of API call will be loaded here...</code></pre>
+		<pre id="result">Results of API call will be loaded here...</pre>
 	</div>
 </div>
