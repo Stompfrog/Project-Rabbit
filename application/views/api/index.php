1c4f9d4256f12af64ff432405f9665ee00c3c593
Chris Bewick <chris@stompfrog.co.uk>
2012-01-29 11:56:56 +0000

    adding basic ajax demo to api page

--- application/views/api/index.php
+++ application/views/api/index.php
@@ -6,9 +6,10 @@
 		<h2>API</h2>
 		<hr />
 		<h3>What are you after?</h3>
-		<ul>
+		<ul id="api-links">
 			<li>Get the 2 latest artists as JSON = <?php echo anchor('/api/artists/2', '/api/artists/2'); ?></li>
 			<li>Get the artist with id 2 as JSON = <?php echo anchor('/api/artist/2', '/api/artist/2'); ?></li>
 		</ul>
+		<pre><code id="result">Results of API call will be loaded here...</code></pre>
 	</div>
 </div>
