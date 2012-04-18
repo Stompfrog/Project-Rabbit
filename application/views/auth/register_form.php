1889450ad339c0547495ccf65d43f198aa095292
Mark Robson <roppa_uk@hotmail.com>
2012-04-18 15:27:16 +0100

    added/updated auth pages

--- application/views/auth/register_form.php
+++ application/views/auth/register_form.php
@@ -41,12 +41,8 @@ $register = array(
 );
 ?>
 
-
-
-
-
 <div class="page-header">
-	<h1>Welcome to project rabbit!</h1>
+	<h1>Register</h1>
 </div>
 <div class="row">
 	<div class="span10">
@@ -132,10 +128,161 @@ $register = array(
 </div>
 
 
+<?php
+/*
+if ($use_username) {
+        $username = array(
+                'name'  => 'username',
+                'id'    => 'username',
+                'value' => set_value('username'),
+                'maxlength'     => $this->config->item('username_max_length', 'tank_auth'),
+                'size'  => 30,
+        );
+}
+$email = array(
+        'name'  => 'email',
+        'id'    => 'email',
+        'value' => set_value('email'),
+        'maxlength'     => 80,
+        'size'  => 30,
+);
+$lat = array(
+        'name'  => 'lat',
+        'id'    => 'lat',
+        'value' => set_value('lat'),
+        'maxlength'     => 40,
+        'size'  => 30,
+);
+$lng = array(
+        'name'  => 'lng',
+        'id'    => 'lng',
+        'value' => set_value('lng'),
+        'maxlength'     => 40,
+        'size'  => 30,
+);
+$password = array(
+        'name'  => 'password',
+        'id'    => 'password',
+        'value' => set_value('password'),
+        'maxlength'     => $this->config->item('password_max_length', 'tank_auth'),
+        'size'  => 30,
+);
+$confirm_password = array(
+        'name'  => 'confirm_password',
+        'id'    => 'confirm_password',
+        'value' => set_value('confirm_password'),
+        'maxlength'     => $this->config->item('password_max_length', 'tank_auth'),
+        'size'  => 30,
+);
+$captcha = array(
+        'name'  => 'captcha',
+        'id'    => 'captcha',
+        'maxlength'     => 8,
+);
+$register = array(
+        'name' => 'register',
+        'id' => 'register',
+        'class' => 'btn primary'
+);
+?>
+<div class="page-header">
+        <h1>Register now</h1>
+</div>
+<div class="row">
+        <div class="span10">
+                <h2>Register</h2>
+                <hr />
+                <?php echo form_open($this->uri->uri_string()); ?>
+                <fieldset>
+                                <div class="clearfix">
+                                        <?php echo form_label('Username', $username['id']); ?>
+                                        <div class="input">
+                                                <?php echo form_input($username); ?>
+                                                <?php echo form_error($username['name']); ?><?php echo isset($errors[$username['name']])?$errors[$username['name']]:''; ?>
+                                        </div>
+                                </div>
+                                <div class="clearfix">
+                                        <?php echo form_label('Email Address', $email['id']); ?>
+                                        <div class="input">
+                                                <?php echo form_input($email); ?>
+                                                <?php echo form_error($email['name']); ?><?php echo isset($errors[$email['name']])?$errors[$email['name']]:''; ?>
+                                        </div>
+                                </div>
+                                <div id="map" style="width:500px; height:300px"></div>
+                                <div class="clearfix">
+                                        <div class="input">
+                                                <?php echo form_label('Latitude', $lat['id']); ?>
+                                                <div class="input">
+                                                        <?php echo form_input($lat); ?>
+                                                </div>
+                                                <?php echo form_label('Longitude', $lng['id']); ?>
+                                                <div class="input">
+                                                        <?php echo form_input($lng); ?>
+                                                </div>
+                                                <label>Address: </label><input id="address"  type="text"/>
+                                        </div>
+                                </div>
+                                <div class="clearfix">
+                                        <?php echo form_label('Password', $password['id']); ?>
+                                        <div class="input">
+                                                <?php echo form_password($password); ?>
+                                                <?php echo form_error($password['name']); ?>
+                                        </div>
+                                </div>
+                                <div class="clearfix">
+                                        <?php echo form_label('Confirm Password', $confirm_password['id']); ?>
+                                        <div class="input">
+                                                <?php echo form_password($confirm_password); ?>
+                                                <?php echo form_error($confirm_password['name']); ?>
+                                        </div>
+                                </div>
+                                <div class="clearfix">
+                                <?php if ($captcha_registration) { ?>
+                                        <?php if ($use_recaptcha) { ?>
+                                                <div id="recaptcha_image"></div>
+                                                <a href="javascript:Recaptcha.reload()">Get another CAPTCHA</a>
+                                                <div class="recaptcha_only_if_image"><a href="javascript:Recaptcha.switch_type('audio')">Get an audio CAPTCHA</a></div>
+                                                <div class="recaptcha_only_if_audio"><a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a></div>
 
 
-
-
-
-
-
+                                                <div class="clearfix">
+                                                        <label class="recaptcha_only_if_image" for="recaptcha_response_field">Enter the words above</label>
+                                                        <label class="recaptcha_only_if_audio" for="recaptcha_response_field">Enter the numbers you hear</label>
+                                                        <div class="input">
+                                                                <input type="text" id="recaptcha_response_field" name="recaptcha_response_field" />     
+                                                                <?php echo form_error('recaptcha_response_field'); ?>
+                                                        </div>
+                                                </div>                                  
+                                                <?php echo $recaptcha_html; ?>
+                                        <?php } else { ?>
+                                                <div class="clearfix">
+                                                        <div class="input">
+                                                                <p>Enter the code exactly as it appears:</p>
+                                                                <?php echo $captcha_html; ?>
+                                                        </div>
+                                                </div>
+                                                <div class="clearfix">
+                                                        <?php echo form_label('Confirmation Code', $captcha['id']); ?>
+                                                        <div class="input">
+                                                                <?php echo form_input($captcha); ?>
+                                                                <?php echo form_error($captcha['name']); ?>
+                                                        </div>
+                                                </div>
+                                        <?php } ?>
+                                <?php } ?>
+                        </fieldset>
+                        <div class="actions">
+                                <?php echo form_submit($register, 'Register'); ?>
+                        </div>
+                <?php echo form_close(); ?>
+        </div>
+        <div class="span4">
+                <h3>Already a member?</h3>
+                <ul class="events unstyled">
+                        <li><a href="<?php echo base_url(); ?>index.php/auth/login" type="submit">Log in</a></li>
+                        <li><?php echo anchor('/auth/forgot_password/', 'Forgotten password?'); ?></li>
+                </ul>
+        </div>
+</div>
+*/
+?>
--- application/views/auth/update_profile.php
+++ application/views/auth/update_profile.php
@@ -1,35 +1,35 @@
 <?php
 $first_name = array(
-    'name'  => 'first_name',
-    'id'    => 'first_name',
-    'value' => set_value('first_name', $table_values['first_name']),
-    'maxlength'     => 30,
-    'size'  => 10,
+	'name'  => 'first_name',
+	'id'    => 'first_name',
+	'value' => set_value('first_name', $table_values['first_name']),
+	'maxlength'     => 30,
+	'size'  => 10,
 );
 $last_name = array(
-    'name'  => 'last_name',
-    'id'    => 'last_name',
-    'value' => set_value('last_name', $table_values['last_name']),
-    'maxlength'     => 30,
-    'size'  => 20,
+	'name'  => 'last_name',
+	'id'    => 'last_name',
+	'value' => set_value('last_name', $table_values['last_name']),
+	'maxlength'     => 30,
+	'size'  => 20,
 );
 $about_me = array(
-    'name'  => 'about_me',
-    'id'    => 'about_me',
-    'value' => set_value('about_me', $table_values['about_me']),
-    'maxlength'     => 80,
-    'size'  => 30,
+	'name'  => 'about_me',
+	'id'    => 'about_me',
+	'value' => set_value('about_me', $table_values['about_me']),
+	'maxlength'     => 80,
+	'size'  => 30,
 );
 $sex = array(
-    'id'    => 'sex',
-    'value' => set_value('sex', $table_values['sex'])
+	'id'    => 'sex',
+	'value' => set_value('sex', $table_values['sex'])
 );
 $address = array(
-    'name'  => 'address',
-    'id'    => 'address',
-    'value' => set_value('address'),
-    'maxlength'     => 100,
-    'size'  => 30,
+	'name'  => 'address',
+	'id'    => 'address',
+	'value' => set_value('address'),
+	'maxlength'     => 100,
+	'size'  => 30,
 );
 $lat = array(
     'name'  => 'lat',
@@ -38,10 +38,10 @@ $lat = array(
     'maxlength'     => 40,
     'size'  => 30,
 );
-$lng = array(
-    'name'  => 'lng',
-    'id'    => 'lng',
-    'value' => set_value('lng'),
+$lon = array(
+    'name'  => 'lon',
+    'id'    => 'lon',
+    'value' => set_value('lon'),
     'maxlength'     => 40,
     'size'  => 30,
 );
@@ -59,88 +59,88 @@ $submit = array(
 );
 ?>
 <div class="page-header">
-        <h1>Welcome to project rabbit!</h1>
+	<h1>Welcome to project rabbit!</h1>
 </div>
 <div class="row">
-        <div class="span10">
-                <h2>Edit your profile</h2>
-                <hr />
-                <?php if (isset($message)) { echo $message; } ?>
-                <?php echo form_open($this->uri->uri_string()); ?>
-                <fieldset>
-                                <div class="clearfix">
-                                        <?php echo form_label('First name', $first_name['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_input($first_name); ?>
-                                                <?php echo form_error($first_name['name']); ?>
-                                                <?php echo isset($errors[$first_name['name']])?$errors[$first_name['name']]:''; ?>
-                                        </div>
-                                </div>
-                                <div class="clearfix">
-                                        <?php echo form_label('Last name', $last_name['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_input($last_name); ?>
-                                                <?php echo form_error($last_name['name']); ?><?php echo isset($errors[$last_name['name']])?$errors[$last_name['name']]:''; ?>
-                                        </div>
-                                </div>
-                                <div class="clearfix">
-                                        <?php echo form_label('About me', $about_me['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_textarea($about_me); ?>
-                                                <?php echo form_error($about_me['name']); ?><?php echo isset($errors[$about_me['name']])?$errors[$about_me['name']]:''; ?>
-                                        </div>
-                                </div>
-                                <div class="clearfix">
-                                        <?php echo form_label('Address', $address['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_input($address); ?>
-                                        </div>
-                                </div>
-                                <div id="map" style="width:100%; height:300px; margin-bottom: 20px; "></div>
-                                <div class="clearfix">
-                                        <?php echo form_label('Latitude', $lat['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_input($lat); ?>
-                                        </div>
-                                </div>
-                                <div class="clearfix">
-                                        <?php echo form_label('Longitude', $lng['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_input($lng); ?>
-                                        </div>
-                                </div>
-                                <div class="clearfix">
-                                        <?php echo form_label('Website', $website['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_input($website); ?>
-                                        </div>
-                                </div>
-                                <div class="clearfix">
-                                        <?php 
-                                                $options = array(
-                                                  'o' => 'Prefer not to say',
-                                  'm' => 'Male',
-                                  'f' => 'Female'
-                                );
-                                         ?>
-                                        <?php echo form_label('Sex', $sex['id']); ?>
-                                        <div class="input">
-                                                <?php echo form_dropdown('sex', $options, $sex['value'], 'id="sex"'); ?>
-                                        </div>
-                                </div>
-                        </fieldset>
-                        <div class="actions">
-                                <?php echo form_submit($submit, 'Update profile'); ?>
-                        </div>
-                <?php echo form_close(); ?>
-        </div>
-        <div class="span4">
-                <h3>Other options</h3>
-                <ul class="events unstyled">
-                        <li><?php echo anchor('/artists/render/', 'View profile'); ?></li>
-                        <li><?php echo anchor('/auth/change_password/', 'Change password'); ?></li>
-                        <li><?php echo anchor('/auth/logout/', 'Logout'); ?></li>
-                        <li><?php echo anchor('/auth/unregister/', 'Unregister'); ?></li>
-                </ul>
-        </div>
+	<div class="span10">
+		<h2>Edit your profile</h2>
+		<hr />
+		<?php if (isset($message)) { echo $message; } ?>
+		<?php echo form_open($this->uri->uri_string()); ?>
+			<fieldset>
+				<div class="clearfix">
+				    <?php echo form_label('First name', $first_name['id']); ?>
+				    <div class="input">
+			            <?php echo form_input($first_name); ?>
+			            <?php echo form_error($first_name['name']); ?>
+			            <?php echo isset($errors[$first_name['name']])?$errors[$first_name['name']]:''; ?>
+				    </div>
+				</div>
+				<div class="clearfix">
+				    <?php echo form_label('Last name', $last_name['id']); ?>
+				    <div class="input">
+			            <?php echo form_input($last_name); ?>
+			            <?php echo form_error($last_name['name']); ?><?php echo isset($errors[$last_name['name']])?$errors[$last_name['name']]:''; ?>
+				    </div>
+				</div>
+				<div class="clearfix">
+				    <?php echo form_label('About me', $about_me['id']); ?>
+				    <div class="input">
+			            <?php echo form_textarea($about_me); ?>
+			            <?php echo form_error($about_me['name']); ?><?php echo isset($errors[$about_me['name']])?$errors[$about_me['name']]:''; ?>
+				    </div>
+				</div>
+				<div class="clearfix">
+				    <?php echo form_label('Address', $address['id']); ?>
+				    <div class="input">
+				    	<?php echo form_input($address); ?>
+				    </div>
+				</div>
+				<div id="map" style="width:100%; height:300px; margin-bottom: 20px; "></div>
+				<div class="clearfix">
+				    <?php echo form_label('Latitude', $lat['id']); ?>
+				    <div class="input">
+				    	<?php echo form_input($lat); ?>
+				    </div>
+				</div>
+				<div class="clearfix">
+				    <?php echo form_label('Longitude', $lon['id']); ?>
+				    <div class="input">
+				    	<?php echo form_input($lon); ?>
+				    </div>
+				</div>
+				<div class="clearfix">
+				    <?php echo form_label('Website', $website['id']); ?>
+				    <div class="input">
+				    	<?php echo form_input($website); ?>
+				    </div>
+				</div>
+				<div class="clearfix">
+				    <?php 
+						$options = array(
+						'o' => 'Prefer not to say',
+						'm' => 'Male',
+						'f' => 'Female'
+						);
+					?>
+				    <?php echo form_label('Sex', $sex['id']); ?>
+				    <div class="input">
+				    	<?php echo form_dropdown('sex', $options, $sex['value'], 'id="sex"'); ?>
+				    </div>
+				</div>
+	        </fieldset>
+			<div class="actions">
+				<?php echo form_submit($submit, 'Update profile'); ?>
+			</div>
+		<?php echo form_close(); ?>
+	</div>
+	<div class="span4">
+		<h3>Other options</h3>
+		<ul class="events unstyled">
+			<li><?php echo anchor('/artists/' . $this->tank_auth->get_user_id(), 'View profile'); ?></li>
+			<li><?php echo anchor('/auth/change_password/', 'Change password'); ?></li>
+			<li><?php echo anchor('/auth/logout/', 'Logout'); ?></li>
+			<li><?php echo anchor('/auth/unregister/', 'Unregister'); ?></li>
+		</ul>
+	</div>
 </div>
